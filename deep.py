ca75bfbff4febb93e2b0f7b2affc50ce2c09852b
Tim0828 <167786372+Tim0828@users.noreply.github.com>
2024-05-21 13:39:06 +0200

    Added plots to visualize the loss (cost) during training. Also I don't seem to be able to reach higher test results than 0.933, perhaps thats due to the lack of data cleaning

--- deep.py
+++ deep.py
@@ -1,24 +1,10 @@
 import numpy as np
 import keras
 from keras import layers
+import matplotlib.pyplot as plt
 
-
-# # input
-# inputs = keras.Input(shape=(210,))
-
-# # new node in the graph of layers
-# dense = layers.Dense(64, activation="relu")
-# x = dense(inputs)
-
-# # another node
-# x = layers.Dense(64, activation="relu")(x)
-# outputs = layers.Dense(2)(x)
-
-# # model
-# model = keras.Model(inputs=inputs, outputs=outputs, name="model")
-
-# # summary
-# model.summary()
+BATCH_SIZE = 64
+EPOCHS = 30
 
 # training data 
 import pandas as pd
@@ -44,11 +30,11 @@ from keras.callbacks import EarlyStopping
 model = keras.Sequential(
     [
         keras.Input(shape=(210,)),
+        layers.Dense(256, activation="relu"),
+        Dropout(0.2),
         layers.Dense(128, activation="relu"),
-        Dropout(0.3),
-        layers.Dense(128, activation="relu"),
-        Dropout(0.3),
-        layers.Dense(128, activation="relu"),
+        Dropout(0.1),
+        layers.Dense(64, activation="relu"),
         layers.Dense(2, activation="softmax"),
     ]
 )
@@ -57,26 +43,32 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy', 
               metrics=['accuracy'])
 
-model.summary()
+# model.summary()
 # add early stopping (prevents overfitting)
-early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+early_stopping = EarlyStopping(monitor='val_loss', patience=15)
 
-history = model.fit(x_train, y_train, batch_size=64, epochs=30, validation_split=0.2, callbacks=[early_stopping])
+history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.2, callbacks=[early_stopping])
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 test_scores = model.evaluate(x_test, y_test)
 print("Test loss:", test_scores[0])
 print("Test accuracy:", test_scores[1])
-model.save("my_model.keras")
-
-# model.compile(
-#     loss=keras.losses.CategoricalCrossentropy(from_logits=True),
-#     optimizer=keras.optimizers.RMSprop(),
-#     metrics=["accuracy"],
-# )
-
-# history = model.fit(x_train, y_train, batch_size=64, epochs=2, validation_split=0.2)
-
-# test_scores = model.evaluate(x_test, y_test)
-# print("Test loss:", test_scores[0])
-# print("Test accuracy:", test_scores[1])
 # model.save("my_model.keras")

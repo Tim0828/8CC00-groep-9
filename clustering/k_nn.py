aade98c2b7ac2576b2c019384207fe1b6496d3e0
Illeep <d.nguyen@student.tue.nl>
2024-06-12 20:44:36 +0200

    Balanced accuracy added

--- clustering/k_nn.py
+++ clustering/k_nn.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, balanced_accuracy_score, classification_report
 
 # reading in the data:
 data = pd.read_csv('PCA_data.csv')
@@ -12,11 +12,13 @@ data['inhibition_class'] = (data['PKM2_inhibition'] * 2 + data['ERK2_inhibition'
 
 # separate features and inhibition
 X_data = data.drop(columns=["PKM2_inhibition", "ERK2_inhibition", "inhibition_class"])  
-Y_data = data["inhibition_class"]  
-
+Y_data_combined = data["inhibition_class"]  
+Y_PKM2 = data["PKM2_inhibition"]
+Y_ERK2 = data["ERK2_inhibition"]
 
 # split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_data, Y_data, test_size=0.4, random_state=42)
+X_train_PKM2, X_test_PKM2, Y_train_PKM2, Y_test_PKM2 = train_test_split(X_data, Y_PKM2, test_size=0.4, random_state=42)
+X_train_ERK2, X_test_ERK2, Y_train_ERK2, Y_test_ERK2 = train_test_split(X_data, Y_ERK2, test_size=0.4, random_state=42)
 
 def Knearestneighbour(train_features, test_features, train_targets, test_targets):
         """k nearest neighbour algorithm 
@@ -29,7 +31,7 @@ def Knearestneighbour(train_features, test_features, train_targets, test_targets
         """
 
         # train KNN classifier
-        knn = KNeighborsClassifier(n_neighbors=4) 
+        knn = KNeighborsClassifier(n_neighbors=2) 
         knn.fit(train_features, train_targets)
 
         # predict on the test set
@@ -37,12 +39,16 @@ def Knearestneighbour(train_features, test_features, train_targets, test_targets
 
         # confusion matrix
         accuracy = accuracy_score(test_targets, pred_targets)
+        balanced_accuracy = balanced_accuracy_score(test_targets, pred_targets)
         cm = confusion_matrix(test_targets, pred_targets)
-        cmd = ConfusionMatrixDisplay(cm, display_labels=[0, 1, 2, 3])
+        cmd = ConfusionMatrixDisplay(cm,)
         cmd.plot(cmap=plt.cm.Blues)
         plt.title("Confusion Matrix")
         plt.show()
         print(f"Accuracy: {accuracy:.2f}")
+        print(f"Balanced accuracy: {balanced_accuracy:.2f}")
+        print(classification_report(test_targets, pred_targets))
 
-Knearestneighbour(X_train, X_test, y_train, y_test)
+Knearestneighbour(X_train_PKM2, X_test_PKM2, Y_train_PKM2, Y_test_PKM2)
+Knearestneighbour(X_train_ERK2, X_test_ERK2, Y_train_ERK2, Y_test_ERK2)
 
